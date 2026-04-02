2a3daa598dcdf5b7a43d6835da1be13904d2d5a6
ClinicalML Developer <developer@clinicalml.com>
2026-04-02 22:58:27 +0530

    Add firebase-config.js with Firebase configuration

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCSBKsHeC2OlgU1MVvBl5PtUNfFXeeQu2Q",
+  authDomain: "clinicalml-fbc92.firebaseapp.com",
+  projectId: "clinicalml-fbc92",
+  storageBucket: "clinicalml-fbc92.firebasestorage.app",
+  messagingSenderId: "311480173596",
+  appId: "1:311480173596:web:c5bd1dd10b19fd2396cbd9",
+  measurementId: "G-T176T5DCTC"
+};
